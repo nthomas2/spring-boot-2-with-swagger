4727e1e1c9bf9eb03ccddf8fcf428b89f3649876
Nathan Thomas <nate.p.thomas@gmail.com>
2019-04-09 16:33:01 -0400

    Initial commit

---
+++ src/main/java/com/nthomas/springboot2swaggerexample/Springboot2swaggerexampleApplication.java
@@ -0,0 +1,13 @@
+package com.nthomas.springboot2swaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2swaggerexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2swaggerexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthomas/springboot2swaggerexample/Springboot2swaggerexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nthomas.springboot2swaggerexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2swaggerexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
